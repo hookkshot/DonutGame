1815bb2f36680735ffdfd2a2e45da486370daa90
Keirron Stach <stachkeirron@gmail.com>
2014-09-29 15:23:34 +0930

    Jump Smoke

---
+++ Unity/Assets/Scripts/Effect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Effect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool alive = false;
+	    
+        if(audio != null)
+        {
+            if (audio.isPlaying)
+                alive = true;
+        }
+
+        if(particleSystem != null)
+        {
+            if (particleSystem.isPlaying)
+                alive = true;
+        }
+
+        if(!alive)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
