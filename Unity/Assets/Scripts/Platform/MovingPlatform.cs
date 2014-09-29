f19be1e350d51afeca5d44acd7af605a3260c3fb
stodt003 <daniel.stone1994@gmail.com>
2014-09-29 15:38:54 +0930

    Moving platform

---
+++ Unity/Assets/Scripts/Platform/MovingPlatform.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public float rightEdge = 4.00f;
+	public float leftEdge = -4.00f;
+	public float topEdge = 4.00f;
+	public float bottomEdge = -4.00f;
+	public float rotationSpeed = 0.00f;
+
+
+	public float horizontalSpeed = 1;
+	private float horizontalDirection = 1;
+
+	public float verticalSpeed = 1;
+	private float verticalDirection = 1;
+
+	// Use this for initialization
+	void Start () {
+		rightEdge += transform.position.x;
+		leftEdge += transform.position.x;
+		topEdge += transform.position.y;
+		bottomEdge += transform.position.y;
+		horizontalDirection = horizontalSpeed;
+		verticalDirection = verticalSpeed;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate() {
+
+		if (transform.position.x > rightEdge) {
+			horizontalDirection = horizontalSpeed*-1;
+				}
+		if (transform.position.x < leftEdge) {
+			horizontalDirection = horizontalSpeed;
+		}
+		if (transform.position.y > topEdge) {
+			verticalDirection = verticalSpeed*-1;
+		}
+		if (transform.position.y < bottomEdge) {
+			verticalDirection = verticalSpeed;
+		}
+		transform.position += new Vector3 (horizontalDirection * Time.deltaTime, verticalDirection * Time.deltaTime, 0);
+		transform.Rotate (new Vector3 (0, 0, rotationSpeed));
+		transform.
+	
+	}
+}
