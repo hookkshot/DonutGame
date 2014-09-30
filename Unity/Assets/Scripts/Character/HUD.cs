396fa4a2bbf65ec611b98023789c4e8e82054007
Keirron Stach <stachkeirron@gmail.com>
2014-09-30 14:39:21 +0930

    Hud 1

--- Unity/Assets/Scripts/Character/HUD.cs
+++ Unity/Assets/Scripts/Character/HUD.cs
@@ -4,15 +4,28 @@ using System.Collections;
 
 public class HUD : MonoBehaviour {
 
-    public Text HealthText;
+    public RectTransform[] PlayerPanels;
+    public GameObject[] PlayerHUD;
+
+    public GameObject PlayerPrefab;
 
 	void Awake()
     {
 
     }
 
-    public void UpdateHealth(float h, float m)
+    void Start()
+    {
+        PlayerHUD = new GameObject[PlayerPanels.Length];
+        for(int i = 0; i < Game.ActivePlayerCount(); i++)
+        {
+            PlayerHUD[i] = (GameObject)GameObject.Instantiate(PlayerPrefab);
+        }
+    }
+
+    public void UpdateHealth(int player, float h, float m)
     {
         //HealthText.text = (int)h + " / " + (int)m;
     }
+
 }
