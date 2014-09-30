a2a7ece8588cee2257f4cebd4f9970b056c055fc
stodt003 <daniel.stone1994@gmail.com>
2014-09-30 14:11:58 +0930

    Falling block damage

--- Unity/Assets/Scripts/Platform/FallingBlock.cs
+++ Unity/Assets/Scripts/Platform/FallingBlock.cs
@@ -3,9 +3,30 @@ using System.Collections;
 
 public class FallingBlock : MonoBehaviour {
 
+	public DamageType Damage;
+
 	// Use this for initialization
 	void Start () {
 	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)	{
+		
+		if (other.gameObject.layer == LayerMask.NameToLayer ("Character")) {
+			
+			HealthSystem h = other.gameObject.GetComponent<HealthSystem>();
+			if(h != null){
+				
+				h.TakeDamage(Damage, gameObject);
+				Destroy(gameObject);
+				
+			}
+			
+		}
+		
+		
+		
+		
 	}
 	
 	// Update is called once per frame
