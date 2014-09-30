e52c17617fad332ce12e90da1d1a63f4ad653048
stodt003 <daniel.stone1994@gmail.com>
2014-09-30 14:11:58 +0930

    Falling block damage

--- Unity/Assets/Scripts/Platform/FallingSpawner.cs
+++ Unity/Assets/Scripts/Platform/FallingSpawner.cs
@@ -6,14 +6,35 @@ public class FallingSpawner : MonoBehaviour {
 	public float Interval = 5;
 	public GameObject Prefab; 
 	private float lastSpawn = 0;
-
+	public DamageType Damage;
 
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	void OnTriggerEnter2D(Collider2D other)	{
+
+		if (other.gameObject.layer == LayerMask.NameToLayer ("Character")) {
+
+			HealthSystem h = other.gameObject.GetComponent<HealthSystem>();
+			if(h != null){
+
+				h.TakeDamage(Damage, gameObject);
+				Destroy(gameObject);
+
+			}
+
+		}
+		
+
+
+
+	}
+
+
+
 	// Update is called once per frame
 	void Update () {
 		if (Time.time > lastSpawn + Interval) {
