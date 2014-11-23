655e2f837fd2175dfbccaeaacabfe25be44529df
Keirron Stach <stachkeirron@gmail.com>
2014-11-23 11:17:35 +1030

    Icing Top
    The new way we will be doing icing. Not implemented yet.

---
+++ Unity/Assets/Scripts/IcingTop.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IcingTop : MonoBehaviour {
+
+    float percent = 0;
+
+    private Animator animator;
+    
+    //Timer Settings
+    private float fadeSync = 0f;
+    private float fadeInterval = 0.1f;
+
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(fadeSync > Time.time)
+        {
+            if(percent > 0)
+                percent -= 0.25f;
+
+            fadeSync = Time.time+fadeInterval;
+            UpdateAnimator();
+        }
+	}
+
+    public void AddIcing()
+    {
+        percent += 10;
+        if (percent > 100)
+            percent = 100;
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+        animator.SetFloat("Percent", percent);
+    }
+}
